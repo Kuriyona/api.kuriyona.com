a915ea46c47d2eecccc9d5328f8f99df8e887ef7
Kuriyona <Kuriyona@outlook.com>
2026-06-30 01:02:12 +0800

    验证 JWT

--- src/router/ask-box.ts
+++ src/router/ask-box.ts
@@ -2,10 +2,11 @@ import Elysia, { t } from "elysia";
 import { askBoxTable } from "../db/schema";
 import { db } from "../utils";
 import { eq } from "drizzle-orm";
+import { validateJWT } from "../plugin/auth";
 
 const app = new Elysia({ prefix: "/ask-box" });
 
-app.post(
+app.use(validateJWT).post(
   "/",
   async ({ body, headers }) => {
     const ip = headers["x-forwarded-for"] || headers["x-real-ip"] || "127.0.0.1";
